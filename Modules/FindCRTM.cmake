d373657604759ec50eedb48cd5f329de3d139247
Mark A Potts <Mark.Potts@noaa.gov>
2019-10-10 17:00:08 +0000

    updated version

--- Modules/FindCRTM.cmake
+++ Modules/FindCRTM.cmake
@@ -6,7 +6,7 @@ if(DEFINED ENV{CRTM} )
   set(CRTM_INC $ENV{CRTM_INC} CACHE STRING "CRTM_8 Library Location")
   set( CRTM_LIB_PATH ${CRTM_LIB} CACHE STRING "CRTM Library Location" )
 else()
-  set(CRTM_VER 2.2.3)
+  set(CRTM_VER 2.3.0)
   find_library( CRTM_LIB 
     NAMES libcrtm_v${CRTM_VER}.a 
     HINTS 
