d6d834fa1c2023248308643cffb4bb08fd3a9856
Dom Heinzeller <climbfuji@ymail.com>
2020-02-04 08:12:35 -0700

    Add Modules/FindWGRIB2.cmake

---
+++ Modules/FindWGRIB2.cmake
@@ -0,0 +1,20 @@
+# This module looks for the wgrib2 library and Fortran modules in WGRIB2_ROOT
+if(DEFINED ENV{WGRIB2_ROOT})
+  set(WGRIB2_ROOT $ENV{WGRIB2_ROOT})
+endif()
+
+find_path (WGRIB2_INCLUDES
+  wgrib2api.mod
+  HINTS ${WGRIB2_ROOT}/include)
+
+find_library (WGRIB2_LIBRARIES
+  names libwgrib2.a
+  HINTS ${WGRIB2_ROOT}/lib ${WGRIB2_ROOT}/lib64)
+
+if(EXISTS ${WGRIB2_INCLUDES} AND EXISTS ${WGRIB2_LIBRARIES})
+  message(STATUS "Found WGRIB2: include directory ${WGRIB2_INCLUDES}, libary ${WGRIB2_LIBRARIES}")
+else()
+  message(STATUS "Unable to locate WGRIB2 library and/or Fortran modules")
+endif()
+
+mark_as_advanced (WGRIB2_INCLUDES WGRIB2_LIBRARIES)
