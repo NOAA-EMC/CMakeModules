e61238fd40e1a8599f72d3a3346380a2621611d2
Mark A Potts <Mark.Potts@noaa.gov>
2020-02-26 17:17:12 +0000

    returned kind extension to gfsio_path

--- Modules/Findgfsio.cmake
+++ Modules/Findgfsio.cmake
@@ -1,7 +1,7 @@
 # This module looks for environment variables detailing where GFSIO lib is
 # If variables are not set, GFSIO will be built from external source
 if(DEFINED ENV{GFSIO_LIB4} )
-  set(GFSIO_LIB4 $ENV{GFSIO_LIB4} CACHE STRING "GFSIO Library Location" )
+  set(GFSIO_LIB4 $ENV{GFSIO_LIB4} CACHE STRING "GFSIO_4 Library Location" )
   set(GFSIO_INC4 $ENV{GFSIO_INC4} CACHE STRING "GFSIO_4 Include Location" )
 
   set(name "gfsio")
@@ -12,20 +12,20 @@ if(DEFINED ENV{GFSIO_LIB4} )
 
   set(kinds "4")
   foreach(kind ${kinds})
-    set(lib_name ${name})
-    set(versioned_lib_name ${name}_${version}_${kinds})
+    set(lib_name ${name}_${kind})
+    set(versioned_lib_name ${name}_${version}_${kind})
 
     if(EXISTS ${${uppercase_name}_LIB${kind}} )
       get_filename_component(lib_dir ${${uppercase_name}_LIB${kind}} DIRECTORY)
-      find_library(gfsio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
+      find_library(gfsio_path_${kind} NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
     
       add_library(${lib_name} STATIC IMPORTED)
       set_target_properties(${lib_name} PROPERTIES
-        IMPORTED_LOCATION ${gfsio_path}
+        IMPORTED_LOCATION ${gfsio_path}_${kind}
         INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
     endif()
   endforeach()
 endif()
 
 find_package_handle_standard_args(gfsio
-  REQUIRED_VARS gfsio_path)
+  REQUIRED_VARS gfsio_path_4)
