d059d55dcab1bc00a76d513c4998c5120a46a5c2
Kyle Gerheiser <kgerheiser@icloud.com>
2020-05-05 14:06:37 -0400

    Fixes from Rahul

--- Modules/FindNetCDF.cmake
+++ Modules/FindNetCDF.cmake
@@ -27,7 +27,6 @@
 #   - C                           - C interface to NetCDF          (netcdf)
 #   - CXX                         - CXX4 interface to NetCDF       (netcdf_c++4)
 #   - Fortran                     - Fortran interface to NetCDF    (netcdff)
-#   - CXX_LEGACY                  - Legacy C++ interface to NetCDF (netcdf_c++)
 #
 # For each component the following are defined:
 #
@@ -63,7 +62,7 @@
 #   - If no components are defined, all components will be searched
 #
 
-list( APPEND _possible_components C CXX Fortran CXX_LEGACY )
+list( APPEND _possible_components C CXX Fortran )
 
 ## Include names for each component
 set( NetCDF_C_INCLUDE_NAME          netcdf.h )
@@ -73,7 +72,6 @@ set( NetCDF_Fortran_INCLUDE_NAME    netcdf.mod )
 ## Library names for each component
 set( NetCDF_C_LIBRARY_NAME          netcdf )
 set( NetCDF_CXX_LIBRARY_NAME        netcdf_c++4 )
-set( NetCDF_CXX_LEGACY_LIBRARY_NAME netcdf_c++ )
 set( NetCDF_Fortran_LIBRARY_NAME    netcdff )
 
 ## Enumerate search components
@@ -83,7 +81,7 @@ foreach( _comp ${_possible_components} )
   set( _name_${_COMP} ${_comp} )
 endforeach()
 
-unset( _search_components )
+set( _search_components C)
 foreach( _comp ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS} )
   string( TOUPPER "${_comp}" _COMP )
   set( _arg_${_COMP} ${_comp} )
@@ -92,9 +90,7 @@ foreach( _comp ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS} )
     message(SEND_ERROR "Find${CMAKE_FIND_PACKAGE_NAME}: COMPONENT ${_comp} is not a valid component. Valid components: ${_possible_components}" )
   endif()
 endforeach()
-if( NOT _search_components )
-  set( _search_components C)
-endif()
+list( REMOVE_DUPLICATES _search_components )
 
 ## Search hints for finding include directories and libraries
 foreach( _comp IN ITEMS "_" "_C_" "_Fortran_" "_CXX_" )
@@ -107,7 +103,13 @@ foreach( _comp IN ITEMS "_" "_C_" "_Fortran_" "_CXX_" )
     endforeach()
   endforeach()
 endforeach()
-list(APPEND _search_hints ${NETCDF} $ENV{NETCDF}) #Old-school HPC module env variable names
+#Old-school HPC module env variable names
+foreach( _name IN ITEMS NetCDF4 NetCDF NETCDF4 NETCDF )
+  foreach( _comp IN ITEMS "_C" "_Fortran" "_CXX" )
+    list(APPEND _search_hints ${${_name}}         $ENV{${_name}})
+    list(APPEND _search_hints ${${_name}${_comp}} $ENV{${_name}${_comp}})
+  endforeach()
+endforeach()
 
 ## Find headers for each component
 set(NetCDF_INCLUDE_DIRS)
@@ -123,7 +125,7 @@ foreach( _comp IN LISTS _search_components )
     PATH_SUFFIXES include include/netcdf
   )
   mark_as_advanced(NetCDF_${_comp}_INCLUDE_FILE)
-# message(DEBUG "NetCDF_${_comp}_INCLUDE_FILE: ${NetCDF_${_comp}_INCLUDE_FILE}")
+  message(DEBUG "NetCDF_${_comp}_INCLUDE_FILE: ${NetCDF_${_comp}_INCLUDE_FILE}")
   if( NetCDF_${_comp}_INCLUDE_FILE )
     get_filename_component(NetCDF_${_comp}_INCLUDE_FILE ${NetCDF_${_comp}_INCLUDE_FILE} ABSOLUTE)
     get_filename_component(NetCDF_${_comp}_INCLUDE_DIR ${NetCDF_${_comp}_INCLUDE_FILE} DIRECTORY)
@@ -135,27 +137,29 @@ if(NetCDF_INCLUDE_DIRS)
 endif()
 set(NetCDF_INCLUDE_DIRS "${NetCDF_INCLUDE_DIRS}" CACHE STRING "NetCDF Include directory paths" FORCE)
 
-## Find nc-config, nf-config and ncxx4-config executables
-list(APPEND _conflist "c" "f" "cxx4")
-list(APPEND _complist "C" "Fortran" "CXX")
-foreach( _conf IN LISTS _conflist )
-  list(FIND _conflist ${_conf} _indx)
-  list(GET  _complist ${_indx} _comp)
+## Find n*-config executables for search components
+foreach( _comp IN LISTS _search_components )
+  if( _comp MATCHES "^(C)$" )
+    set(_conf "c")
+  elseif( _comp MATCHES "^(Fortran)$" )
+    set(_conf "f")
+  elseif( _comp MATCHES "^(CXX)$" )
+    set(_conf "cxx4")
+  endif()
   find_program( NetCDF_${_comp}_CONFIG_EXECUTABLE
       NAMES n${_conf}-config
     HINTS ${NetCDF_INCLUDE_DIRS} ${_include_search_hints} ${_search_hints}
     PATH_SUFFIXES bin Bin ../bin ../../bin
       DOC "NetCDF n${_conf}-config helper" )
+    message(DEBUG "NetCDF_${_comp}_CONFIG_EXECUTABLE: ${NetCDF_${_comp}_CONFIG_EXECUTABLE}")
 endforeach()
-unset(_conflist)
-unset(_complist)
 
 set(_C_libs_flag --libs)
 set(_Fortran_libs_flag --flibs)
 set(_CXX_libs_flag --libs)
-set(_C_flags_flag --includedir)
-set(_Fortran_flags_flag --includedir)
-set(_CXX_flags_flag --includedir)
+set(_C_includes_flag --includedir)
+set(_Fortran_includes_flag --includedir)
+set(_CXX_includes_flag --includedir)
 function(netcdf_config exec flag output_var)
   set(${output_var} False PARENT_SCOPE)
   if( exec )
@@ -180,7 +184,7 @@ foreach( _comp IN LISTS _search_components )
   mark_as_advanced( NetCDF_${_comp}_LIBRARY )
   get_filename_component(NetCDF_${_comp}_LIBRARY ${NetCDF_${_comp}_LIBRARY} ABSOLUTE)
   set(NetCDF_${_comp}_LIBRARY ${NetCDF_${_comp}_LIBRARY} CACHE STRING "NetCDF ${_comp} library" FORCE)
-# message(DEBUG "NetCDF_${_comp}_LIBRARY: ${NetCDF_${_comp}_LIBRARY}")
+  message(DEBUG "NetCDF_${_comp}_LIBRARY: ${NetCDF_${_comp}_LIBRARY}")
 
   if( NetCDF_${_comp}_LIBRARY )
     if( NetCDF_${_comp}_LIBRARY MATCHES ".a$" )
@@ -208,7 +212,7 @@ foreach( _comp IN LISTS _search_components )
   endif()
 
   #Use nc-config to set per-component INCLUDE_DIRS variable if possible
-  netcdf_config( ${NetCDF_${_comp}_CONFIG_EXECUTABLE} ${_${_comp}_flags_flag} _val )
+  netcdf_config( ${NetCDF_${_comp}_CONFIG_EXECUTABLE} ${_${_comp}_includes_flag} _val )
   if( _val )
     string( REPLACE " " ";" _val ${_val} )
     set( NetCDF_${_comp}_INCLUDE_DIRS ${_val} )
