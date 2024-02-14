de1067b17a583a8992d0361919dd58a4e4801321
Alex Richert <alexander.richert@noaa.gov>
2024-02-14 10:52:31 -0800

    correct capitalization and place aliases after main target def

--- Modules/FindESMF.cmake
+++ Modules/FindESMF.cmake
@@ -115,17 +115,6 @@ else()
 
 endif()
 
-# Add target aliases to avoid spurious '-l' flags.
-if(NOT TARGET ESMF::ESMF)
-  add_library(ESMF::ESMF ALIAS ESMF)
-endif()
-# The following 'ESMF' target alias can be removed after all UFS component CMake
-# configs transition to using 'ESMF::ESMF'. As it is, it creates backward
-# compatibility with ESMF/MAPL configurations that use the 'ESMF' target.
-if(NOT TARGET ESMF)
-  add_library(ESMF ALIAS ESMF)
-endif()
-
 ## Finalize find_package
 include(FindPackageHandleStandardArgs)
 
@@ -143,4 +132,15 @@ if(ESMF_FOUND)
     IMPORTED_LOCATION "${ESMF_LIBRARY_LOCATION}"
     INTERFACE_INCLUDE_DIRECTORIES "${ESMF_F90COMPILEPATHS}"
     INTERFACE_LINK_LIBRARIES "${ESMF_INTERFACE_LINK_LIBRARIES}")
+endif()
+
+# Add target aliases to avoid spurious '-l' flags.
+if(NOT TARGET ESMF::ESMF)
+  add_library(ESMF::ESMF ALIAS esmf)
+endif()
+# The following 'ESMF' target alias can be removed after all UFS component CMake
+# configs transition to using 'ESMF::ESMF'. As it is, it creates backward
+# compatibility with ESMF/MAPL configurations that use the 'ESMF' target.
+if(NOT TARGET ESMF)
+  add_library(ESMF ALIAS esmf)
 endif()
